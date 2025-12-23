9914725dc0ba3bc441afb72a0be4b0887b613f51
Adam Mathlay <mathlayadam@icloud.com>
2025-12-23 08:37:17 +0200

    immediates persistion issues

--- src/main/kotlin/pipelining/Encode.kt
+++ src/main/kotlin/pipelining/Encode.kt
@@ -3,6 +3,8 @@ package org.cuttlefish.pipelining
 import org.cuttlefish.Parser.toRegister
 import org.cuttlefish.instructions.Instruction
 import org.cuttlefish.instructions.InstructionType
+import kotlin.math.pow
+import kotlin.text.toString
 
 class Encode(val instructStr: String) {
     val name = instructStr.split(" ")[0]
@@ -30,6 +32,9 @@ class Encode(val instructStr: String) {
             InstructionType.Immediates -> {
                 half(0)
                 immediate(instructStr.split(' ')[2].toShort())
+                opcodeOther()
+                halfOther(1)
+                immediateOther(instructStr.split(' ')[2].toShort())
             }
 
             InstructionType.StandAlone -> {}
@@ -46,15 +51,33 @@ class Encode(val instructStr: String) {
         full = code.toUShort()
         full = (full.toInt() shl 16 - 5).toUShort()
     }
+    fun opcodeOther() {
+        val code = (Instruction.mappings[name]!![1] as Number).toByte()
+        full2 = code.toUShort()
+        full2 = (full2.toInt() shl 16 - 5).toUShort()
+    }
 
     fun half(half: Byte) {
         val shift = (half.toInt() shl 16 - 5) shr 1
         full = (full.toInt() or shift).toUShort()
     }
+    fun halfOther(half: Byte) {
+        val shift = (half.toInt() shl 16 - 5) shr 1
+        full2 = (full2.toInt() or shift).toUShort()
+    }
+
 
     fun immediate(number: Short) {
         val shift = ((number.toInt() shl 16 - 5) shr 9) shr 5 + 1
         full = (full.toInt() or shift).toUShort()
+    }
+    fun immediateOther(number: Short) { // 00110100
+        val nNoShift = number.toInt() shr 8 shl 2
+        val mask = (2.0.pow(6) - 1).toInt() shl 16 -6// 63 ?
+//        val shift = nNoShift or mask
+//        println(shift.toString(2).padStart(16, '0'))
+//        val shift = ((number.toInt() shl 16 - 5) shr 9) shr 5 + 1
+        full2 = (full2.toInt() or nNoShift).toUShort()
     }
 
 
@@ -81,6 +104,8 @@ class Encode(val instructStr: String) {
 fun main() {
     val sh = Short.MAX_VALUE / 2 - 3000
     val x = Encode("li r8 $sh") // 11111110000
-    println(x.full.toString(2).padStart(16, '0'))
-    println(x.full)
+    println(x.full.toString(2).padStart(16, '0').removeRange(0..5).removeRange(8,10))
+    println(x.full2.toString(2).padStart(16, '0').removeRange(0..5).removeRange(8,10))
+    println("0011010011010001".toInt(2))
+    println(sh)
 }
