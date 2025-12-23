153c33b74634342f0bdca4feb358fb59cee7e855
Adam Mathlay <mathlayadam@icloud.com>
2025-12-23 12:49:10 +0200

    memory status working ish-y

---
+++ src/main/kotlin/data/Memory.kt
@@ -0,0 +1,27 @@
+package org.cuttlefish.data
+
+
+object Memory {
+    const val SIZE = 256
+    private val memory = ShortArray(SIZE) { -1 }
+    fun write(address: Short, value: Short) {
+        if (validAddress(address)) {
+            memory[address.toInt()] = value
+        }
+    }
+
+    fun read(address: Short): Short {
+        if (!validAddress(address)) {
+            return -1 // Would've thrown an exception!!
+        }
+        return memory[address.toInt()]
+    }
+
+    private fun validAddress(address: Short): Boolean {
+        if (address in 0..<SIZE) {
+            throw IllegalStateException("Out of bounds address!!! ($address)")
+        }
+        return true
+    }
+
+}
